711e9fae914fa13fa882871f54daf1a0012ac4d6
Digital Venture <absar.design@gmail.com>
2025-11-18 18:14:33 +0400

    updated About page

--- g366-assets/js/script.js
+++ g366-assets/js/script.js
@@ -1,5 +1,7 @@
 $(function(){
 
+	// Client Animation
+
     var controller = new ScrollMagic();
 
     var wipeAnimation = new TimelineMax()
@@ -18,6 +20,35 @@ $(function(){
     var scene = new ScrollScene({ triggerElement: "#pinContainer", triggerHook: "onLeave", duration: 10000 })
         .setTween(wipeAnimation)
         .setPin("#pinContainer")
+        .addIndicators()
+        .addTo(controller);
+
+	// About Animation
+
+	var wipeAnimationAbout = new TimelineMax()
+		// .fromTo("section.ab_panel.one .ff_image", 1,{x:  "130%"}, {x: "0%", ease: Linear.easeNone})
+		.fromTo("section.ab_panel.two", 1, {y:  "-100%"}, {y: "0%", ease: Linear.easeNone})
+		.fromTo("section.ab_panel.two .ff_image", 1,{x:  "130%"}, {x: "0%", ease: Linear.easeNone})
+		.fromTo("section.ab_panel.three", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone})
+		.fromTo("section.ab_panel.three .ff_image", 1,{x:  "130%"}, {x: "0%", ease: Linear.easeNone});
+
+	function stepOneComplete(){
+		setTimeout(function(){
+			console.log('Loaded scene 1');
+			$('#pinAbSlide2 #pin').show();
+		},1500);
+	}
+	
+	function stepTwoComplete(){
+		setTimeout(function(){
+			console.log('Loaded scene 2');
+			$('#pinAbSlide3 #pin').show();
+		},1500);
+	}
+
+	var scene2 = new ScrollScene({ triggerElement: "#pinContainer_about", triggerHook: "onLeave", duration: 2000 })
+        .setTween(wipeAnimationAbout)
+        .setPin("#pinContainer_about")
         .addIndicators()
         .addTo(controller);
 
